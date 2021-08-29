51c4af3d6d017d915b6f65af70f14dc8531f3f1d
zetame <66808008+zetame@users.noreply.github.com>
2021-08-29 17:43:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const numbers = [1,2,3,4,5,6];
 const diceNumber = Math.floor(Math.random() * numbers.length);
 
+function playBeep () {
+
+    var audio = document.getElementById("audio1");
+    audio.play();
+}
 
 function roll1 () {
     
@@ -13,6 +18,7 @@ function roll1 () {
         
         document.getElementById("rollbutton1").innerHTML = "1";
         
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -25,7 +31,7 @@ function roll1 () {
         document.getElementById("startdice1").src ="Dice-2-b.svg.png";
         
         document.getElementById("rollbutton1").innerHTML = "2";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -37,7 +43,7 @@ function roll1 () {
         document.getElementById("startdice1").src ="Dice-3-b.svg.png";
        
         document.getElementById("rollbutton1").innerHTML = "3";
-       
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -49,7 +55,7 @@ function roll1 () {
         document.getElementById("startdice1").src ="Dice-4-b.svg.png";
         
         document.getElementById("rollbutton1").innerHTML = "4";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -60,7 +66,7 @@ function roll1 () {
         document.getElementById("startdice1").src ="Dice-5-b.svg.png";
         
         document.getElementById("rollbutton1").innerHTML = "5";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -71,7 +77,7 @@ function roll1 () {
         document.getElementById("startdice1").src ="Dice-6-b.svg.png";
         
         document.getElementById("rollbutton1").innerHTML = "6";
-       
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -90,7 +96,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-1-b.png";
         
         document.getElementById("rollbutton2").innerHTML = "1";
-       
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -103,7 +109,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-2-b.svg.png";
         
         document.getElementById("rollbutton2").innerHTML = "2";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -115,7 +121,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-3-b.svg.png";
        
         document.getElementById("rollbutton2").innerHTML = "3";
-       
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -127,7 +133,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-4-b.svg.png";
         
         document.getElementById("rollbutton2").innerHTML = "4";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -138,7 +144,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-5-b.svg.png";
         
         document.getElementById("rollbutton2").innerHTML = "5";
-       
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
@@ -149,7 +155,7 @@ function roll2 () {
         document.getElementById("startdice2").src ="Dice-6-b.svg.png";
         
         document.getElementById("rollbutton2").innerHTML = "6";
-        
+        playBeep ();
         
         setTimeout(() => {  location.reload(); }, 7000);
 
