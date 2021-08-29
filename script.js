9b2633752e7fade77904829e3a20354abc7992af
zetame <66808008+zetame@users.noreply.github.com>
2021-08-29 03:35:13 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
+const numbers = [1,2,3,4,5,6];
+const diceNumber = Math.floor(Math.random() * numbers.length);
 
-function roll () {
+
+function roll1 () {
     
-    const numbers = [1,2,3,4,5,6];
-    const diceNumber = Math.floor(Math.random() * numbers.length);
+    
 
     if (diceNumber==0) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/1/1b/Dice-1-b.svg";
+        document.getElementById("startdice1").src ="Dice-1-b.png";
         
-        document.getElementById("rollbutton").innerHTML = "1";
+        document.getElementById("rollbutton1").innerHTML = "1";
         play1();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -20,9 +22,9 @@ function roll () {
 
     else if (diceNumber==1) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Dice-2-b.svg/1200px-Dice-2-b.svg.png";
+        document.getElementById("startdice1").src ="Dice-2-b.svg.png";
         
-        document.getElementById("rollbutton").innerHTML = "2";
+        document.getElementById("rollbutton1").innerHTML = "2";
         play2();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -32,9 +34,9 @@ function roll () {
 
     else if (diceNumber==2) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/768px-Dice-3-b.svg.png";
+        document.getElementById("startdice1").src ="Dice-3-b.svg.png";
        
-        document.getElementById("rollbutton").innerHTML = "3";
+        document.getElementById("rollbutton1").innerHTML = "3";
         play3();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -44,9 +46,9 @@ function roll () {
 
     else if (diceNumber==3) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Dice-4-b.svg/1024px-Dice-4-b.svg.png";
+        document.getElementById("startdice1").src ="Dice-4-b.svg.png";
         
-        document.getElementById("rollbutton").innerHTML = "4";
+        document.getElementById("rollbutton1").innerHTML = "4";
         play4();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -55,9 +57,9 @@ function roll () {
 
     else if (diceNumber==4) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Dice-5-b.svg/1200px-Dice-5-b.svg.png";
+        document.getElementById("startdice1").src ="Dice-5-b.svg.png";
         
-        document.getElementById("rollbutton").innerHTML = "5";
+        document.getElementById("rollbutton1").innerHTML = "5";
         play5();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -66,9 +68,9 @@ function roll () {
 
     else if (diceNumber==5) {
 
-        document.getElementById("startdice").src ="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/768px-Dice-6-b.svg.png";
+        document.getElementById("startdice1").src ="Dice-6-b.svg.png";
         
-        document.getElementById("rollbutton").innerHTML = "6";
+        document.getElementById("rollbutton1").innerHTML = "6";
         play6();
         
         setTimeout(() => {  location.reload(); }, 7000);
@@ -77,6 +79,85 @@ function roll () {
 
 
 }
+
+
+function roll2 () {
+    
+    
+
+    if (diceNumber==0) {
+
+        document.getElementById("startdice2").src ="Dice-1-b.png";
+        
+        document.getElementById("rollbutton2").innerHTML = "1";
+        play1();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+
+
+    }
+
+    else if (diceNumber==1) {
+
+        document.getElementById("startdice2").src ="Dice-2-b.svg.png";
+        
+        document.getElementById("rollbutton2").innerHTML = "2";
+        play2();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+
+    }
+
+    else if (diceNumber==2) {
+
+        document.getElementById("startdice2").src ="Dice-3-b.svg.png";
+       
+        document.getElementById("rollbutton2").innerHTML = "3";
+        play3();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+
+    }
+
+    else if (diceNumber==3) {
+
+        document.getElementById("startdice2").src ="Dice-4-b.svg.png";
+        
+        document.getElementById("rollbutton2").innerHTML = "4";
+        play4();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+    }
+
+    else if (diceNumber==4) {
+
+        document.getElementById("startdice2").src ="Dice-5-b.svg.png";
+        
+        document.getElementById("rollbutton2").innerHTML = "5";
+        play5();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+    }
+
+    else if (diceNumber==5) {
+
+        document.getElementById("startdice2").src ="Dice-6-b.svg.png";
+        
+        document.getElementById("rollbutton2").innerHTML = "6";
+        play6();
+        
+        setTimeout(() => {  location.reload(); }, 7000);
+
+    }
+
+
+}
+
 
   
 function play1() {
